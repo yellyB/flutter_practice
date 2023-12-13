63cfd5f45264f7702c605954e2c8443f493e203b
yelly <kk3p1q1@gmail.com>
2023-12-13 23:26:02 +0900

    결과 화면: 점수

--- lib/screen/screen_result.dart
+++ lib/screen/screen_result.dart
@@ -28,16 +28,66 @@ class ResultScreen extends StatelessWidget {
         ),
         body: Center(
           child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                border: Border.all(color: Colors.deepPurple),
-                color: Colors.deepPurple,
-              ),
-              width: width * 0.85,
-              height: height * 0.5,
-              child: Column(
-                children: <Widget>[],
-              )),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(color: Colors.deepPurple),
+              color: Colors.deepPurple,
+            ),
+            width: width * 0.85,
+            height: height * 0.5,
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(bottom: width * 0.048),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(color: Colors.deepPurple),
+                    color: Colors.white,
+                  ),
+                  width: width * 0.73,
+                  height: height * 0.25,
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.fromLTRB(
+                          0,
+                          width * 0.048,
+                          0,
+                          width * 0.012,
+                        ),
+                        child: Text('수고하셨습니다!',
+                            style: TextStyle(
+                              fontSize: width * 0.055,
+                              fontWeight: FontWeight.bold,
+                            )),
+                      ),
+                      Text(
+                        '당신의 점수는',
+                        style: TextStyle(
+                          fontSize: width * 0.048,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(),
+                      ),
+                      Text('${score.toString()}/${quizs.length.toString()}',
+                          style: TextStyle(
+                            fontSize: width * 0.21,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.red,
+                          )),
+                      Padding(
+                        padding: EdgeInsets.all(width * 0.012),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
